609d5963ba9b0fe77b9260f8100a20e71106b5d3
尚伟维 <shangweiwei365@hellobike.com>
2021-10-15 14:22:37 +0800

    fix: 修改

--- test.py
+++ test.py
@@ -1,25 +1,5 @@
-# def gcd(a,b):
-#     if a%b == 0:
-#         return b
-#     return gcd(b, a%b)
-
-# n = int(input());
-# for i in range(n):
-#     ans = 0;
-#     one = int(input())
-#     for j in range(1, one+1):
-#         if j == gcd(j, one) and (one / j) != j:
-#             if (one / j)%j != 0 or j == 1:
-#                 ans += 1
-#     print(int(ans/2))
-
-# n = int(input())
-# def ans():
-#     k = int(input())
-#     for j in range(k,1,reversed=True):
-#         for l in str(j):
-
-# for i in range(n):
-#     ans()
-for j in range(1,10,2,reversed=True):
-    print(j)
+import requests #导入爬虫的库，不然调用不了爬虫的函数
+response = requests.get("http://www.baidu.com")  #生成一个response对象
+response.encoding = response.apparent_encoding #设置编码格式
+print("状态码:"+ str( response.status_code ) ) #打印状态码
+print(response.text)#输出爬取的信息
