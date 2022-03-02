61c5399c963853b4a25eedfedca694c8c7c07974
尚伟维 <shangweiwei@sobey.com>
2022-03-02 18:27:09 +0800

    fix: 实习报告

--- summary/index.js
+++ summary/index.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 request('https://www.baidu.com/', function (err, response, body) {
-  if (!err && response.statusCode == 200) { 
-    console.log(body)
+  if (!err && response.statusCode == 200) {
+    console.log('hello world');
   }
 })
